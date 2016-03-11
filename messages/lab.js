0d09c365a736e45ec8ca371695e3872fc783e601
Elizabeth <Elizabeth@Elizabeths-MacBook-Air-3.local>
2016-03-11 00:03:53 -0500

    adding lab 6 files

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+	request.onreadystatechange = function() {
+		console.log("Hello!");
+		if (request.readyState == 4) {
+			console.log("Got the data back!");
+			data = request.responseText;
+			console.log(data);
+			msg = JSON.parse(data);
+			elem = document.getElementById("messages");
+			for (i = 0; i < msg.length; i++) {
+				elem.innerHTML += "<p>" + msg[i]["content"] + " - " + msg[i]["username"] + "</p>";
+			}
+		}
+	};
+	request.send(null);
+}
